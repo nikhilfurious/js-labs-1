6e8ee3efa12917da3b97c2bc2012274f92fc2219
Kumanan Murugesan <kumanan12@gmail.com>
2020-11-29 10:57:59 -0500

    amortization calculator

---
+++ amortization-calculator.js
@@ -0,0 +1,32 @@
+//A = P * ( r(1+r) ^n/ (1=r)^n-1
+
+// monthly EMI
+// P - Principal amount
+// R - interest rate in years
+// N - number of years
+function calculateAmountPerPeriod(P, R, N) {        
+    let r = (R/100)/12; 
+    let n = N*12;
+    console.log(`r is ${r}, n is ${n} and P is ${P}`);
+     let numerator = r * Math.pow((1+r),n);
+     let denominator = Math.pow((1+r),n) -1;
+     let amount = P * (numerator/denominator);
+     console.log(`amount is ${amount}`);
+     return amount;
+    
+}
+
+var P = 20000;
+var R= 7.5;
+var N = 5;
+
+var emi = calculateAmountPerPeriod(P, R, N);
+
+function calculate() {
+    var P = document.getElementById("P").value;
+    var R= document.getElementById("R").value;
+    var N = document.getElementById("N").value;
+    var finalAmount = document.getElementById("finalAmount");
+    var amoutPerPeriod = calculateAmountPerPeriod(P, R, N);
+    finalAmount.innerText = amoutPerPeriod.toFixed(2);
+}
