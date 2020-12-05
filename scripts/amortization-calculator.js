3c6ea3a4e273921cc3a6ec3711dc32c80e9fdd74
“Kumanan <kumanan12@gmail.com>
2020-12-05 18:28:39 -0500

    Refactored code.

--- scripts/amortization-calculator.js
+++ scripts/amortization-calculator.js
@@ -5,14 +5,20 @@
 // R - interest rate in years
 // N - number of years
 function calculateAmountPerPeriod(P, R, N) {        
- return 0;   
+    let r = (R/100)/12; 
+    let n = N*12;
+    console.log(`r is ${r}, n is ${n} and P is ${P}`);
+     let numerator = r * Math.pow((1+r),n);
+     let denominator = Math.pow((1+r),n) -1;
+     let amount = P * (numerator/denominator);
+     console.log(`amount is ${amount}`);
+     return amount;   
 }
 
 var P = 20000;
 var R= 7.5;
 var N = 5;
 
-var emi = calculateAmountPerPeriod(P, R, N);
 
 function calculate() {
     var P = document.getElementById("P").value;
